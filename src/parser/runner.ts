80ea7cd09db25f79cc635a40c7590cbe6e9899e0
filipe <filipe.soares@biologix.com.br>
2025-08-03 11:44:33 -0300

    concating :O

--- src/parser/runner.ts
+++ src/parser/runner.ts
@@ -1,5 +1,6 @@
 import {
   L3Base,
+  L3Expression,
   L3ExpressionStatement,
   L3LibraryMethod,
   L3Method,
@@ -9,6 +10,7 @@ import {
   L3Reference,
   L3Statement,
   L3String,
+  L3StringConcat,
   Runnable,
 } from './l3-types';
 
@@ -21,57 +23,57 @@ export class Runner {
     this.r = r;
   }
 
-  resolveReference(obj: L3Base) {
-    if (!(obj instanceof L3Reference)) {
-      return obj;
-    }
-    return this.r.symbols[obj.name];
+  resolveSymbol(name: string) {
+    return this.r.symbols[name];
   }
 
-  readValue(obj: L3Base) {
+  runExpression(obj: L3Expression) {
     if (obj instanceof L3String) {
       return obj.value;
     }
     if (obj instanceof L3Number) {
       return obj.value;
     }
+    if (obj instanceof L3Reference) {
+      return obj.name;
+    }
+    if (obj instanceof L3Operation) {
+      return this.runOperation(obj);
+    }
+
     throw new Error(`Cannot read value from ${obj}`);
   }
 
   runOperation(op: L3Operation) {
-    let current = op.operand;
+    let current: any = this.runExpression(op.operand);
     for (const step of op.steps) {
       if (step instanceof L3MethodCall) {
-        current = this.resolveReference(current);
+        const ref = this.resolveSymbol(current);
         const argList: any[] = [];
         for (const arg of step.argList) {
-          argList.push(this.readValue(arg));
+          argList.push(this.runExpression(arg));
         }
-        if (current instanceof L3LibraryMethod) {
-          current.callback(argList, this);
-        } else if (current instanceof L3Method) {
-          this.runStatementList(current.statements);
+        if (ref instanceof L3LibraryMethod) {
+          ref.callback(argList, this);
+        } else if (ref instanceof L3Method) {
+          this.runStatementList(ref.statements);
         } else {
-          throw new Error(`Cannot run ${current}`);
+          throw new Error(`Cannot run ${ref}`);
         }
+      } else if (step instanceof L3StringConcat) {
+        const other = this.runExpression(step.other);
+        current += other;
       } else {
         throw new Error(`Unknown step ${step}`);
       }
     }
-  }
-
-  runExpression(obj: L3ExpressionStatement) {
-    if (obj.expr instanceof L3Operation) {
-      this.runOperation(obj.expr);
-    } else {
-      throw new Error(`Cannot run ${obj.expr}`);
-    }
+    return current;
   }
 
   runStatementList(list: L3Statement[]) {
     for (const item of list) {
       if (item instanceof L3ExpressionStatement) {
-        this.runExpression(item);
+        this.runExpression(item.expr);
       } else {
         throw new Error(`Unknown statement ${item}`);
       }
