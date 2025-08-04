d1c8ec2f2bbed039d22280e45d19d3e8fe063deb
filipe <filipe.soares@biologix.com.br>
2025-08-04 13:28:04 -0300

    added license header

--- src/parser/l2-parser.ts
+++ src/parser/l2-parser.ts
@@ -1,3 +1,23 @@
+/**
+ * Capy project.
+ * Copyright (c) 2025 - Filipe Vilela Soares
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file Layer-2 parser implementation.
+ */
+
 import { Base, ParseError } from './base';
 import { L1Bracket, L1Operator, L1String, L1Base, L1Word, L1Number, L1BasePos } from './l1-types';
 import {
